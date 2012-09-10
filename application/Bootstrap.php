b63dcfbb5f29358386aebf89402aebec059b48a9
Matt Munsey <matt@kentriv.com>
2012-09-10 00:29:31 -0700

    Updated Bootstrap file

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -12,16 +12,11 @@
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
- * @category   Zend
- * @package    Zend_Cloud
- * @subpackage examples
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
 /**
- * @category   Zend
- * @package    Zend_Cloud
  * @subpackage examples
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
